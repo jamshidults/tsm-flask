2909534196245e1391723bafab34255ea3065350
jamshidk <jamshid.kakunderi@ultsglobal.com>
2024-07-22 15:07:46 +0530

    [ADD] added flask_cors lib

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify,render_template
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy import desc
+from flask_cors import CORS
 
 from models import OrderModel
 from db import db
@@ -9,6 +10,7 @@ from resource.orders import blp as OrderBlueprint
 
 def create_app(db_url=None):
     app = Flask(__name__)
+    CORS(app)
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or "sqlite:///tsm_data_backup.db"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
